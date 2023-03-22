e44bd58e44b3de06082c235ef03526f7da308786
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-03-22 18:57:09 +0530

    created user schema

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -33,7 +34,10 @@ const storage = multer.diskStorage({
 const upload=multer({storage}); 
 
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"),register);
 
+/* MONGODB SET-UP */ 
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -43,8 +47,6 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+   
   })
   .catch((error) => console.log(`${error} did not connect`));
