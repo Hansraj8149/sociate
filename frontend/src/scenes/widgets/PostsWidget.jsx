2a4d40720b0a68077c4473941ed230c5d1b17d85
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-05-30 19:50:59 +0530

    in postswidget  added the fake posts so that if post from database did not load they can be display

--- frontend/src/scenes/widgets/PostsWidget.jsx
+++ frontend/src/scenes/widgets/PostsWidget.jsx
@@ -5,28 +5,125 @@ import PostWidget from "./PostWidget";
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts);
+  const posts = useSelector((state) => state.posts) || [
+    {
+      _id: "6475cd71b39f2a144ef93f6a",
+      userId: "6475cd71b39f2a144ef93f64",
+      firstName: "Harvey",
+      lastName: "Dunn",
+      location: "Los Angeles, CA",
+      description:
+        "This is the last really long random description. This one is longer th…",
+      picturePath: "post4.jpeg",
+      userPicturePath: "p7.jpeg",
+
+      likes: {},
+
+      comments: [
+        "I lied again, one more random comment",
+
+        "Why am I doing this?",
+
+        "I'm bored",
+        "I'm still bored",
+        "All I want to do is play video games",
+        "I'm going to play video games",
+      ],
+    },
+  {
+    
+_id:
+"6475cd71b39f2a144ef93f6b",
+userId:
+"6475cd71b39f2a144ef93f65",
+firstName:
+"Carly",
+lastName:
+"Vowel",
+location:
+"Chicago, IL",
+description:
+"Just a short description. I'm tired of typing. I'm going to play video…",
+picturePath:
+"post5.jpeg",
+userPicturePath:
+"p8.jpeg",
+
+likes: {},
+
+comments:
+[
+"I lied again, one more random comment"
+,
+"Why am I doing this?"
+,
+"Man I'm bored"
+,
+"What should I do?"
+,
+"I'm going to play video games"],
+  },
+  {
+    _id: "6475cd71b39f2a144ef93f6a",
+    userId: "6475cd71b39f2a144ef93f64",
+    firstName: "Harvey",
+    lastName: "Dunn",
+    location: "Los Angeles, CA",
+    description:
+      "This is the last really long random description. This one is longer th…",
+    picturePath: "post4.jpeg",
+    userPicturePath: "p7.jpeg",
+
+    likes: {},
+
+    comments: [
+      "I lied again, one more random comment",
+
+      "Why am I doing this?",
+
+      "I'm bored",
+      "I'm still bored",
+      "All I want to do is play video games",
+      "I'm going to play video games",
+    ],
+  },
+
+  ]; // Initialize as empty array if null or undefined
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
+    try {
+      const response = await fetch("http://localhost:3001/posts", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch posts");
+      }
+      const data = await response.json();
+      dispatch(setPosts(data));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const getUserPosts = async () => {
-    const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
-      {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
+    try {
+      const response = await fetch(
+        `http://localhost:3001/posts/${userId}/posts`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch user posts");
       }
-    );
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
+      const data = await response.json();
+      dispatch(setPosts(data));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -36,6 +133,10 @@ const PostsWidget = ({ userId, isProfile = false }) => {
       getPosts();
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // if (!Array.isArray(posts)) {
+  //   return null; // Render null or a loading indicator while posts are being fetched
+  // }
 
   return (
     <>
