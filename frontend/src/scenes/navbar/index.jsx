9cd68a7a2c53d15ec157e50d7cc1f288a4030dcd
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-05-30 15:59:03 +0530

    did some updates

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Sociate
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
