ef33f8ef55238fdf82bf0b208380809766b450a1
Hansraj8149 <hansrajsaini8149@gmail.com>
2024-03-05 17:56:02 +0530

    vercel.json file added successfully

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-/* CONFIGURATIONS */
+/* nONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -48,12 +48,17 @@ app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
+
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get('/',(req,res)=> {
+  res.send('welocome to sociate backend')
+})
+
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
